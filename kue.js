9ff1f60f636ca671eb7946ff1bddc33b5af013a1
Edward Chiang <edwardubuntu@gmail.com>
2016-07-20 13:00:22 +0800

    Edit redis to go setting

--- kue.js
+++ kue.js
@@ -1,8 +1,13 @@
 var kue = require('kue');
+var redisUrl =  require("url").parse(process.env.REDISTOGO_URL);
 
-var jobs = kue.createQueue({
-  redis: process.env.REDISTOGO_URL
-});
+var redisOptions = {
+    host: redisUrl.hostname,
+    port: parseInt(redisUrl.port),
+    auth: redisUrl.auth.split(":")[1]
+}
+
+var jobs = kue.createQueue(redisOptions);
 
 // see https://github.com/learnBoost/kue/ for how to do more than one job at a time
 jobs.process('crawl', function(job, done) {
