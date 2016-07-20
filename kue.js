d31c1c07eda74ed445f0d643352b0997c1620697
Edward Chiang <edwardubuntu@gmail.com>
2016-07-20 12:47:48 +0800

    Change connection

--- kue.js
+++ kue.js
@@ -1,20 +1,8 @@
-var kue = require('kue')
-  , url = require('url');
+var kue = require('kue');
 
-var kueOptions = {};
-
-if (process.env.REDISTOGO_URL) {
-    var redisUrl = url.parse(process.env.REDISTOGO_URL);
-    kueOptions.redis = {
-        port: parseInt(redisUrl.port),
-        host: redisUrl.hostname
-    };
-    if(redisUrl.auth) {
-        kueOptions.redis.auth = redisUrl.auth.split(':')[1];
-    }
-}
-
-var jobs = kue.createQueue(kueOptions);
+var jobs = kue.createQueue({
+  redis: process.env.REDISTOGO_URL
+});
 
 // see https://github.com/learnBoost/kue/ for how to do more than one job at a time
 jobs.process('crawl', function(job, done) {
