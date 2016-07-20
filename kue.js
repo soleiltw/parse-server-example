13e054dc969a959347e50898b429bc39a536f04d
Edward Chiang <edwardubuntu@gmail.com>
2016-07-20 12:44:25 +0800

    Change create queue from REDISTOGO_URL

--- kue.js
+++ kue.js
@@ -1,21 +1,20 @@
 var kue = require('kue')
-  , url = require('url')
-  , redis = require("redis");
+  , url = require('url');
 
-// make sure we use the Heroku Redis To Go URL
-// (put REDISTOGO_URL=redis://localhost:6379 in .env for local testing)
+var kueOptions = {};
 
-kue.redis.createClient = function() {
-    console.log("process.env.REDISTOGO_URL: " + process.env.REDISTOGO_URL);
-    var redisUrl = url.parse(process.env.REDISTOGO_URL)
-      , client = redis.createClient(redisUrl.port, redisUrl.hostname);
-    if (redisUrl.auth) {
-        client.auth(redisUrl.auth.split(":")[1]);
+if (process.env.REDISTOGO_URL) {
+    var redisUrl = url.parse(process.env.REDISTOGO_URL);
+    kueOptions.redis = {
+        port: parseInt(redisUrl.port),
+        host: redisUrl.hostname
+    };
+    if(redisUrl.auth) {
+        kueOptions.redis.auth = redisUrl.auth.split(':')[1];
     }
-    return client;
-};
+}
 
-var jobs = kue.createQueue();
+var jobs = kue.createQueue(kueOptions);
 
 // see https://github.com/learnBoost/kue/ for how to do more than one job at a time
 jobs.process('crawl', function(job, done) {
