d3da73d0940da541f0cf9d772062245bee4f28b0
Edward Chiang <edwardubuntu@gmail.com>
2016-07-20 13:11:03 +0800

    Setup only redis url

--- kue.js
+++ kue.js
@@ -1,13 +1,20 @@
-var kue = require('kue');
-var redisUrl =  require("url").parse(process.env.REDISTOGO_URL);
+var kue = require('kue'),
+  url = require('url');
 
-var redisOptions = {
-    host: redisUrl.hostname,
-    port: parseInt(redisUrl.port),
-    auth: redisUrl.auth.split(":")[1]
+var kueOptions = {};
+
+if(process.env.REDISTOGO_URL) {
+    var redisUrl = url.parse(process.env.REDISTOGO_URL);
+    kueOptions.redis = {
+        port: parseInt(redisUrl.port),
+        host: redisUrl.hostname
+    };
+    if(redisUrl.auth) {
+        kueOptions.redis.auth = redisUrl.auth.split(':')[1];
+    }
 }
 
-var jobs = kue.createQueue(redisOptions);
+var jobs = kue.createQueue(kueOptions);
 
 // see https://github.com/learnBoost/kue/ for how to do more than one job at a time
 jobs.process('crawl', function(job, done) {
