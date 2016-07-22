1834b92bd19800c729da37f06df527ee1995e69c
Edward Chiang <edwardubuntu@gmail.com>
2016-07-22 11:48:26 +0800

    Setup kue for push to care taker

---
+++ kue-example.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// Using Heroku Scheduler with Node.js http://stackoverflow.com/questions/13345664/using-heroku-scheduler-with-node-js
+function sayHello() {
+	console.log('Hello from kue.js');
+}
+sayHello();
+
+// An example for - In Node.js, how do I “include” functions from my other files? http://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
+var tools = require('./cloud/tools');
+tools.foo()
+tools.bar()
+console.log(typeof tools.zemba);
+
+process.exit();
--- kue.js
+++ kue.js
@@ -1,15 +1,74 @@
 #!/usr/bin/env node
 
-// Using Heroku Scheduler with Node.js http://stackoverflow.com/questions/13345664/using-heroku-scheduler-with-node-js
-function sayHello() {
-	console.log('Hello from kue.js');
-}
-sayHello();
+var main = require('./cloud/main');
 
-// An example for - In Node.js, how do I “include” functions from my other files? http://stackoverflow.com/questions/5797852/in-node-js-how-do-i-include-functions-from-my-other-files
-var tools = require('./cloud/tools');
-tools.foo()
-tools.bar()
-console.log(typeof tools.zemba);
+function pushToCaretaker() {
+	var startDate = new Date();
+  startDate.setMinutes(startDate.getMinutes() - 31 );
+  var endDate = new Date();
+  endDate.setMinutes(endDate.getMinutes() - 1 );
+
+  console.log("startDate  = " + startDate.toString());
+  console.log("endDate  = " + endDate.toString());
+
+  // Query for punchCard
+  var punchCardQuery = new Parse.Query("PunchCard");
+  punchCardQuery.greaterThan("estimatedAt",startDate);
+  punchCardQuery.lessThan("estimatedAt",endDate);
+  punchCardQuery.equalTo("status","not_yet"); 
+  punchCardQuery.include("ownerPointer");
+  punchCardQuery.include("prescriptionPointer");
+  punchCardQuery.find({
+        success: function(results){
+
+          console.log("punchCardQuery size = " + results.length);
+          // Send Push for each sellerBroadcast
+          for(var i = 0 ; i < results.length; i++){
+            var punchCard = results[i];
+
+            // Query for Group (= careTaker)
+            var groupQuery = new Parse.Query("Group"); 
+            groupQuery.equalTo("status","verified"); 
+            groupQuery.equalTo("ownerPointer",punchCard.get("ownerPointer")); 
+            groupQuery.include("caretakerPointer");
+            groupQuery.find({
+                success: function(groupResults){
+                  console.log("groupQuery size = " + groupResults.length);
+
+                  // Send push for each careTaker
+                  for(var i = 0 ; i < groupResults.length; i++){
+                    var group = groupResults[i];
+                    var openType = "caretaker";
+                    var openTypeObjectId = punchCard.id;
+                    var recipientUserId = group.get("caretakerPointer").id;
+                    var message = punchCard.get("ownerPointer").get("name") +  " 忘記吃藥了。" ;
+                    var title = "忘記吃藥提醒";
+
+                    // Send Push from cloud code
+                    SendPush(recipientUserId,message,title,openType,openTypeObjectId,{
+                          success: function(returnValue){
+                            console.log("Send Push success");
+                          },
+                          error: function(error){
+                            console.error("Send Push error.");
+                          }
+                    }); 
+                  }
+
+                },
+                error: function(error){
+                  response.error("Find GroupQuery error. " + error);
+                }
+            });
+          }
+          
+        },
+        error: function(error){
+          response.error("Find PunchCard error. " + error);
+        }
+    });
+}
+
+pushToCaretaker()
 
 process.exit();
