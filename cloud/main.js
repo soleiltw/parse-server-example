d6c8014fba199bb96dd1bb1f7d80f8c669abc3c9
Edward Chiang <edwardubuntu@gmail.com>
2016-07-22 14:44:56 +0800

    Setup example

--- cloud/main.js
+++ cloud/main.js
@@ -3,14 +3,3 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi from heroku.');
 });
 
-function sayHello() {
-	var startDate = new Date();
-	startDate.setMinutes(startDate.getMinutes() - 31 );
-	var endDate = new Date();
-	endDate.setMinutes(endDate.getMinutes() - 1 );
-
-	console.log("startDate  = " + startDate.toString());
-	console.log("endDate  = " + endDate.toString());
-
-	
-}
--- task-hello.js
+++ task-hello.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 
-var Parse = require('parse').Parse;
-var main = require('./cloud/main');
-
-main.sayHello();
+var kue = requrire('kue')
+	, queue = kue.createQueue();
+	
+queue.process('say hello', function(job, done) {
+	// Start coding
+	sayHello()
+	
+	done();
+});
 
 process.exit();
+
+function sayHello() {
+	var startDate = new Date();
+	startDate.setMinutes(startDate.getMinutes() - 31 );
+	var endDate = new Date();
+	endDate.setMinutes(endDate.getMinutes() - 1 );
+
+	console.log("startDate  = " + startDate.toString());
+	console.log("endDate  = " + endDate.toString());
+}
