2e8a1217d76e7173c44bed91246f2f4cafae74dd
Edward Chiang <edwardubuntu@gmail.com>
2016-07-22 17:00:01 +0800

    Setup two function

--- cloud/main.js
+++ cloud/main.js
@@ -3,3 +3,22 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi from heroku.');
 });
 
+Parse.Cloud.define('QueryBeaconSpot', function(req, res){
+	var beaconSpot = Parse.Object.extend("BeaconSpot");
+	var query = new Parse.Query(beaconSpot);
+	query.find({
+		success: function(result) {
+		console.log("Successfully retrieved " + results.length + " beacon spot.");
+		
+		for (var i = 0; i < results.length; i++) {
+			var object = results[i];
+			console.log(object.id + ' - ' + object.get('name'));
+    	}
+    	res.success('QueryBeaconSpot success.');
+		
+		}, error: function(error) {
+			console.log("error: "+error);
+			res.error(error);
+		}
+	});
+});
--- task-hello.js
+++ task-hello.js
@@ -8,25 +8,16 @@ Parse.Cloud.useMasterKey();
 // Simple test for hello
 Parse.Cloud.run('hello', {}).then(function(result) {
     // make sure the set the enail sent flag on the object
-    console.log("result :" + JSON.stringify(result));
+    console.log("hello result :" + JSON.stringify(result));
 }, function(error) {
     // error
     console.log("error: "+error);
 });
 
 // Check and log object
-var beaconSpot = Parse.Object.extend("BeaconSpot");
-var query = new Parse.Query(beaconSpot);
-query.find({
-	success: function(result) {
-		console.log("Successfully retrieved " + results.length + " beacon spot.");
-		
-		for (var i = 0; i < results.length; i++) {
-			var object = results[i];
-			console.log(object.id + ' - ' + object.get('name'));
-    	}
-		
-	}, error: function(error) {
-		console.log("error: "+error);
-	}
+Parse.Cloud.run('QueryBeaconSpot', {}).then(function(result) {
+    console.log("QueryBeaconSpot result :" + JSON.stringify(result));
+}, function(error) {
+    // error
+    console.log("error: "+error);
 });
