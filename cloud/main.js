283d0113663438670ca083efad0056b10af3c0f1
Edward Chiang <edwardubuntu@gmail.com>
2016-07-22 12:34:11 +0800

    Remove parse require

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,5 @@
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 
-var Parse = require('parse').Parse;
-
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world! From Heroku.");
