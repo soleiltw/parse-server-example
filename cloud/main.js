0ef3eab97b2c239cfc5b64d8c76fe65ed23d983f
Edward Chiang <edwardubuntu@gmail.com>
2017-03-10 12:35:24 +0800

    Update to email send push

--- cloud/main.js
+++ cloud/main.js
@@ -4,41 +4,39 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define("sendPushToUser", function(request, response) {
-  var senderUser = request.user;
-  var recipientUserId = request.params.recipientId;
-  var message = request.params.message;
-  var title = request.params.title;
-  var activity = request.params.activity;
+  var recipientEmail = request.params.email;
+    var message = request.params.message;
+    var title = request.params.title;
+    var sound = request.params.sound;
  
-  // Validate the message text.
-  // For example make sure it is under 140 characters
-  if (message.length > 140) {
-  // Truncate and add a ...
-    message = message.substring(0, 137) + "...";
-  }
  
-  // Send the push.
-  // Find devices associated with the recipient user
-  var recipientUser = new Parse.User();
-  recipientUser.id = recipientUserId;
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("userPointer", recipientUser);
-  
-  // Send the push notification to results of the query
-  Parse.Push.send({
-    where: pushQuery,
-    data: {
-      alert: message,
-      title: title,
-      activity: activity
+    // Validate the message text.
+    // For example make sure it is under 140 characters
+    if (message.length > 140) {
+        // Truncate and add a ...
+        message = message.substring(0, 137) + "...";
     }
-  }, {
-  	useMasterKey: true,
-        success: function () {
-            response.success("Push was sent successfully.")
-        },
-        error: function (error) {
-            response.error("Push failed to send with error: " + error.message);
+ 
+    // Send the push.
+    // Find devices associated with the recipient user
+
+    var userQuery = new Parse.Query(Parse.User);
+    userQuery.equalTo("email", recipientEmail);
+
+    var pushQuery = new Parse.Query(Parse.Installation);
+    pushQuery.matchesQuery("userPointer", userQuery);
+    // Send the push notification to results of the query
+    Parse.Push.send({
+        where: pushQuery,
+        data: {
+          alert: message,
+          title: title,
+          sound: sound
         }
-  });
+    }).then(function() {
+      // Check if push query
+        response.success("Push success.");
+    }, function(error) {
+        response.error("Push failed to send with error: " + error.message);
+    });
 });
