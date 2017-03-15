f650d237c8963f66a20c70c5659e4040c103ca21
Edward Chiang <edwardubuntu@gmail.com>
2017-03-15 21:48:54 +0800

    Fix error typing

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ var api = new ParseServer({
     android: {
       senderId: process.env.ANDROID_SENDERID || '', // The sender id of GCM
       apiKey: process.env.ANDROID_APIKEY || '' // The Server API Key of GCM
-    }
-
+    }, 
     ios: [{
       pfx: 'certs/ItaowayDevCertificates.p12', // the path and filename to the .p12 file you exported earlier. 
       bundleId: 'tw.org.efarm.itaoway', // The bundle identifier associated with your app
