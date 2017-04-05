c0a43c19ade527aad18417c82c9efe6364c4f323
Edward Chiang <edwardubuntu@gmail.com>
2017-04-05 16:06:21 +0800

    Support push to guid user

--- cloud/main.js
+++ cloud/main.js
@@ -4,7 +4,7 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define("sendPushToUser", function(request, response) {
-  var recipientEmail = request.params.email;
+    var recipientGUID = request.params.guid;
     var message = request.params.message;
     var title = request.params.title;
     var sound = request.params.sound;
@@ -21,7 +21,7 @@ Parse.Cloud.define("sendPushToUser", function(request, response) {
     // Find devices associated with the recipient user
 
     var userQuery = new Parse.Query(Parse.User);
-    userQuery.equalTo("email", recipientEmail);
+    userQuery.equalTo("guid", recipientGUID);
 
     var pushQuery = new Parse.Query(Parse.Installation);
     pushQuery.matchesQuery("userPointer", userQuery);
